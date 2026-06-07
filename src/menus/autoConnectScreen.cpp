3b6623c7caf0370e4472a9ccc8971e813c7386d3
JabberwockPL <24412050+JabberwockPL@users.noreply.github.com>
2026-06-07 20:48:18 +0200

    Update autoConnectScreen.cpp

--- src/menus/autoConnectScreen.cpp
+++ src/menus/autoConnectScreen.cpp
@@ -271,8 +271,8 @@ AutoConnectPosition::AutoConnectPosition(string value)
                 continue;
             }
 
-            pos -= 1;  // Shift for legacy compatibility
             if (pos < 0) pos = 0;
+            pos -= 1;
             if (pos > static_cast<int>(CrewPosition::MAX)) pos = static_cast<int>(CrewPosition::MAX);
             crew_position = CrewPosition(pos);
         }
